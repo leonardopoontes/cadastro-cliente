80537aff05e7f37e492736717b7ee1a93f7e1d00
Your Name <you@example.com>
2023-02-26 11:12:28 -0300

    atualizacao

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 Route::get('/index', [\App\Http\Controllers\ClienteController::class, 'index'])->name('cliente.index');
 Route::get('/create', [\App\Http\Controllers\ClienteController::class, 'create'])->name('cliente.create');
